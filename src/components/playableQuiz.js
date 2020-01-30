81a2a5e354da65da8892ab1fa53a6182c8920f95
btate712 <btate712@gmail.com>
2020-01-29 19:25:45 -0600

    Added review at end of quiz

--- src/components/playableQuiz.js
+++ src/components/playableQuiz.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PlayableQuestion from './playableQuestion';
+import Question from './question';
 
 
 class PlayableQuiz extends React.Component {
   askQuestionOrGiveSummary = () => {
-    console.log("current question: ", this.props.quiz.currentQuestion);
     if(this.props.quiz.currentQuestion <= this.props.quiz.questions.length - 1) {
       return(
-        <div>
+        <>
           <h1>Question #{this.props.quiz.currentQuestion + 1}:</h1>
           <PlayableQuestion question={this.props.quiz.questions[this.props.quiz.currentQuestion]} />
-        </div>
+        </>
       )
     } else {
       return (
-        <h1>
-          Quiz Complete!
-        </h1>
+        <>
+          {this.showSummary()}
+        </>
       )
     }
   }
+
+  numberToLetter(number) {
+    switch (number) {
+      case 1:
+        return "A";
+      case 2:
+        return "B";
+      case 3:
+        return "C";
+      case 4:
+        return "D";
+      default:
+        return "Not Found"
+    }  
+  }
+
+  showSummary = () => {
+    const output = this.props.quiz.questions.map((question, index) => {
+      return(
+        <>
+          <h1>Question #{index + 1}</h1>
+          <Question question={question} />
+          <h2>You answered: {this.numberToLetter(question.response)}</h2>
+          <hr />
+        </>
+      )
+    })
+    return(
+      <>
+        {output}
+      </>
+    )
+  }
+
   render() {
     return (
       <div>
