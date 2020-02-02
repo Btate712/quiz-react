a914ebfbf091984773af05a675ce481a5a9abbe5
btate712 <btate712@gmail.com>
2020-02-02 07:05:52 -0600

    implemented redirect after new question submittal

--- src/components/newQuestionForm.js
+++ src/components/newQuestionForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createQuestion } from '../actions/questionActions';
 import { URL } from '../appData/applicationConstants';
+import { Redirect } from 'react-router-dom';
 
 class NewQuestionForm extends Component {
   state = {
@@ -11,7 +12,8 @@ class NewQuestionForm extends Component {
     choice2: "",
     choice3: "",
     choice4: "",
-    correctChoice: 1
+    correctChoice: 1,
+    complete: false
   }
 
   handleInputChange = event => {
@@ -22,8 +24,10 @@ class NewQuestionForm extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    const question = this.state
-    this.props.createQuestion(question, this.props.user.token)
+    const question = this.state;
+    this.props.createQuestion(question, this.props.user.token);
+    this.setState({ complete: true })
+
   }
 
   topicOptions = () => {
@@ -36,46 +40,55 @@ class NewQuestionForm extends Component {
     )
   }
 
+  redirectWhenComplete = () => {
+    if (this.state.complete === true) {
+      return (<Redirect to={`/topics/${this.props.topic.id}`} />)
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label>Topic: </label>
-          <select name="topicId" onChange={this.handleInputChange} value={this.state.topicId} >
-            {this.topicOptions()}
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Question Stem:</label>
-          <input name="stem" id="stem" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.stem} />
-        </div>
-        <div className="form-group">
-          <label>Choice 1:</label>
-          <input name="choice1" id="choice1" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice1} />
-        </div>
-        <div className="form-group">
-          <label>Choice 2:</label>
-          <input name="choice2" id="choice2" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice2} />
-        </div>
-        <div className="form-group">
-          <label>Choice 3:</label>
-          <input name="choice3" id="choice3" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice3} />
-        </div>
-        <div className="form-group">
-          <label>Choice 4:</label>
-          <input name="choice4" id="choice4" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice4} />
-        </div>
-        <div className="form-group">
-          <label>Correct Choice: </label>
-          <select name="correctChoice" onChange={this.handleInputChange} value={this.state.correctChoice} >
-            <option value="1">A</option>
-            <option value="2">B</option>
-            <option value="3">C</option>
-            <option value="4">D</option>
-          </select>
-        </div>
-        <input type="submit" className="btn btn-primary"/>
-      </form>
+      <div className={`${this.state.complete}`}>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Topic: </label>
+            <select name="topicId" onChange={this.handleInputChange} value={this.state.topicId} >
+              {this.topicOptions()}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Question Stem:</label>
+            <textarea name="stem" id="stem" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.stem} />
+          </div>
+          <div className="form-group">
+            <label>Choice 1:</label>
+            <input name="choice1" id="choice1" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice1} />
+          </div>
+          <div className="form-group">
+            <label>Choice 2:</label>
+            <input name="choice2" id="choice2" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice2} />
+          </div>
+          <div className="form-group">
+            <label>Choice 3:</label>
+            <input name="choice3" id="choice3" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice3} />
+          </div>
+          <div className="form-group">
+            <label>Choice 4:</label>
+            <input name="choice4" id="choice4" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.choice4} />
+          </div>
+          <div className="form-group">
+            <label>Correct Choice: </label>
+            <select name="correctChoice" onChange={this.handleInputChange} value={this.state.correctChoice} >
+              <option value="1">A</option>
+              <option value="2">B</option>
+              <option value="3">C</option>
+              <option value="4">D</option>
+            </select>
+          </div>
+          <input type="submit" className="btn btn-primary"/>
+        </form>
+        {this.redirectWhenComplete()}
+      </div>
     );
   }
 }
