b94a074db811128f3ef4406e6559b18c8cc05f2b
btate712 <btate712@gmail.com>
2020-02-05 18:11:07 -0600

    added score and color formatting for incorrect answers to quiz summary

--- src/components/playableQuiz.js
+++ src/components/playableQuiz.js
@@ -58,20 +58,39 @@ class PlayableQuiz extends React.Component {
     }  
   }
 
+  getScore = () => {
+    let numberRight = 0;
+    const questions = this.props.quiz.questions;
+    const outOf = questions.length;
+    questions.forEach(question => {
+      if (question.choice === question.correct_choice) {
+        numberRight += 1;
+      }
+    })
+    return {numberRight: numberRight, outOf: outOf}
+  }
+
   showSummary = () => {
+    const score = this.getScore();
     const output = this.props.quiz.questions.map((question, index) => {
       return(
         <div key={index}>
           <h3>Question Id: {question.id}</h3>
           <h1>Question #{index + 1}: </h1>
           <Question question={question} />
-          <h2>You answered: {this.numberToLetter(question.choice)}</h2>
+          <div className="container">
+            <h2 className={question.choice === question.correct_choice ? "correct" : "incorrect"}>
+              You answered: {this.numberToLetter(question.choice)}
+            </h2>
+          </div>
           <hr />
         </div>
       )
     })
     return(
       <>
+        <h1>{score.numberRight} out of {score.outOf} correct:  {(score.numberRight * 100 / score.outOf).toFixed(2)}%.</h1>
+        <hr />
         {output}
       </>
     )
