4392f89225ae141c0834356be159210b9b250493
btate712 <btate712@gmail.com>
2020-02-09 06:01:13 -0600

    implemented PlayableQuestionDisplay to remove some display code from logic-heavy PlayableQuestion

--- src/components/playableQuestion.js
+++ src/components/playableQuestion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getSelection } from '../actions/quizActions';
 import { numberToLetter } from '../actions/questionActions';
+import PlayableQuestionDisplay from './playableQuestionDisplay';
 
 class PlaybleQuestion extends React.Component {
   state = {
@@ -70,24 +71,12 @@ class PlaybleQuestion extends React.Component {
   }
 
   render() {
-    const question = this.props.question;
     return (
-      <div className="Question container" >
-        <h3>Question Id: {question.id}</h3>
-        <h1 dangerouslySetInnerHTML={{__html: question.stem}} />
-        <br />
-        <h1 id="1" className="ml-5 playable-question-choice" onClick={this.handleSelection}
-          dangerouslySetInnerHTML={{__html: `A. ${question.choice_1}`}} />
-        <h1 id="2" className="ml-5 playable-question-choice" onClick={this.handleSelection}
-          dangerouslySetInnerHTML={{__html: `B. ${question.choice_2}`}} />
-        <h1 id="3" className="ml-5 playable-question-choice" onClick={this.handleSelection}
-          dangerouslySetInnerHTML={{__html: `C. ${question.choice_3}`}} />
-        <h1 id="4" className="ml-5 playable-question-choice" onClick={this.handleSelection}
-          dangerouslySetInnerHTML={{__html: `D. ${question.choice_4}`}} />
-        <br />
-        {this.showGradedQuestion()}        
-        
-      </div>
+      <PlayableQuestionDisplay
+        question={this.props.question} 
+        handleSelection={this.handleSelection}
+        showGradedQuestion={this.showGradedQuestion}
+      />
     )
   }
 }
