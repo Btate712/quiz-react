dd6a5affc2a70cfd598284e5e1a90e82737cd3a4
btate712 <btate712@gmail.com>
2020-02-13 19:21:43 -0600

    added formatting to explanation

--- src/components/comment.js
+++ src/components/comment.js
@@ -4,6 +4,8 @@ const Comment = props => {
   const dateCreated = new Date(props.comment.created_at);
   return(
     <>
+      {console.log(props)}
+      {props.comment.comment_type === "explanation" ? <h2>Explanation:</h2> : <h2>Comment:</h2>}
       <h3>{props.comment.text}</h3>
       <h3>By User: {props.user.name}</h3>
       <h4>{`${dateCreated.getMonth() + 1}/${dateCreated.getDate()}/${dateCreated.getFullYear()}`}</h4>
