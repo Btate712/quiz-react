4d175e572942bd661c9c3e6b18b25c6d4d660eed
btate712 <btate712@gmail.com>
2020-02-13 20:07:45 -0600

    used dangerouslySetHTML to maintain HTML formatting

--- src/components/comment.js
+++ src/components/comment.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Comment = props => {
   const dateCreated = new Date(props.comment.created_at);
+
   return(
     <>
       {console.log(props)}
       {props.comment.comment_type === "explanation" ? <h2>Explanation:</h2> : <h2>Comment:</h2>}
-      <h3>{props.comment.text}</h3>
+      <h3 dangerouslySetInnerHTML={ {__html: props.comment.text} } ></h3>
       <h3>By User: {props.user.name}</h3>
       <h4>{`${dateCreated.getMonth() + 1}/${dateCreated.getDate()}/${dateCreated.getFullYear()}`}</h4>
       <hr />
