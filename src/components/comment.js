4b79ad54a73f940db9f9bed2c11538f24c1d625a
btate712 <btate712@gmail.com>
2020-02-19 14:12:50 -0600

    stopped showing comments of type "stop-showing"

--- src/components/comment.js
+++ src/components/comment.js
@@ -4,15 +4,25 @@ import SanitizedHTML from 'react-sanitized-html'
 const Comment = props => {
   const dateCreated = new Date(props.comment.created_at);
 
-  return(
-    <>
-      {props.comment.comment_type === "explanation" ? <h2>Explanation:</h2> : <h2>Comment:</h2>}
-      <h3><SanitizedHTML html={props.comment.text} /></h3>
-      <h3>By User: {props.user.name}</h3>
-      <h4>{`${dateCreated.getMonth() + 1}/${dateCreated.getDate()}/${dateCreated.getFullYear()}`}</h4>
-      <hr />
-    </>
-  )
+  const show = () => {
+    if(props.comment.comment_type === "stop-asking") {
+      return (<></>)
+    }
+    else {
+      return(
+        <>
+          {props.comment.comment_type === "explanation" ? <h2>Explanation:</h2> : <h2>Comment:</h2>}
+          <h3><SanitizedHTML html={props.comment.text} /></h3>
+          <h3>By User: {props.user.name}</h3>
+          <h4>{`${dateCreated.getMonth() + 1}/${dateCreated.getDate()}/${dateCreated.getFullYear()}`}</h4>
+          <hr />
+        </>
+      )
+    }
+  }
+
+  return show()
+  
 }
 
 export default Comment
