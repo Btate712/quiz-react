1373f5dcf44970dbfb70a1634a5bfa27707420f0
btate712 <btate712@gmail.com>
2020-03-25 12:31:02 -0500

    cleaned up unneeded comment

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,55 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class LoginForm extends React.Component {
-  state = {
+function LoginForm(props) {
+
+  const [input, setInput] = useState({
     username: "",
     password: ""
-  }
+  });
 
-  handleInputChange = event => {
-    this.setState({
+  const handleInputChange = event => {
+    setInput({
+      ...input,
     [event.target.name]: event.target.value
     });
   }
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.login(this.state.username, this.state.password);
-    this.setState({
+    props.login(input.username, input.password);
+    setInput({
       username: "",
       password: ""
     });
   }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-3 col-sm-1"></div>
-          <div className="col-lg-4 col-md-6 col-sm-10">
-            <div className="container border">
-              <h1>User Login</h1>
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                  <label>Username:</label>
-                  <input name="username" id="username" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.username} />
-                </div>
-                <div className="form-group">
-                  <label>Password:</label>
-                  <input name="password" id="password" type="password" className="form-control" onChange={this.handleInputChange} value={this.state.password} />
-                </div>
-                <input type="submit" className="btn btn-primary"/>
-                <Link to="/register"><button className="btn btn-primary pull-right">Register New User</button></Link>
-              </form>
-              <br />
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 col-md-3 col-sm-1"></div>
+        <div className="col-lg-4 col-md-6 col-sm-10">
+          <div className="container border">
+            <h1>User Login</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Username:</label>
+                <input name="username" id="username" type="text" className="form-control" onChange={handleInputChange} value={input.username} />
+              </div>
+              <div className="form-group">
+                <label>Password:</label>
+                <input name="password" id="password" type="password" className="form-control" onChange={handleInputChange} value={input.password} />
+              </div>
+              <input type="submit" className="btn btn-primary"/>
+              <Link to="/register"><button className="btn btn-primary pull-right">Register New User</button></Link>
+            </form>
+            <br />
           </div>
-          <div className="col-lg-4 col-md-3 col-sm-1"></div>
         </div>
+        <div className="col-lg-4 col-md-3 col-sm-1"></div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default LoginForm;
