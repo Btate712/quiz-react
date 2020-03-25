cb8f10f9ffef366b3f5da623d051596b7b3e4dab
btate712 <btate712@gmail.com>
2020-03-25 13:16:57 -0500

    changed to functional component using useInputHooks instead of state

--- src/components/newTopicForm.js
+++ src/components/newTopicForm.js
@@ -1,48 +1,41 @@
 import React from 'react';
 import ConditionalRedirect from './conditionalRedirect';
 import SelectOptions from './selectOptions';
+import { useInputHooks } from '../helperFunctions';
 
-class NewTopicForm extends React.Component {
+function NewTopicForm(props) {
 
-  state = {
+  const [input, handleInputChange, setInput] = useInputHooks({
     name: "",
     complete: false,
     projectId: 1
-  }
+  })
 
-  handleInputChange = event => {
-    this.setState({
-    [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.createTopic(this.state.name, this.state.projectId, this.props.token); 
-    this.setState({complete: true});
+    props.createTopic(input.name, input.projectId, props.token); 
+    setInput({complete: true});
    }
   
-  render() {
-    return(
-      <div className="container" >
-        <h1>New Topic:</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Topic Name:</label>
-            <input name="name" id="name" type="text" className="form-control" onChange={this.handleInputChange} value={this.state.name} />
-          </div>
-          <label>
-            Project: <select name="projectId" onChange={this.handleInputChange} value={this.state.projectId} >
-              <SelectOptions objects={this.props.projects} />
-            </select>
-          </label>
-          <br />
-          <input type="submit" className="btn btn-primary"/>
-        </form>
-        <ConditionalRedirect to="/topics" condition={this.state.complete} />
-      </div>
-    )
-  }
+  return(
+    <div className="container" >
+      <h1>New Topic:</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Topic Name:</label>
+          <input name="name" id="name" type="text" className="form-control" onChange={handleInputChange} value={input.name} />
+        </div>
+        <label>
+          Project: <select name="projectId" onChange={handleInputChange} value={input.projectId} >
+            <SelectOptions objects={props.projects} />
+          </select>
+        </label>
+        <br />
+        <input type="submit" className="btn btn-primary"/>
+      </form>
+      <ConditionalRedirect to="/topics" condition={input.complete} />
+    </div>
+  )
 }
 
 export default NewTopicForm;
